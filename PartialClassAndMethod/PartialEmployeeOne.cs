585343e5619e44cd7df3ddf8c12b17000da5c394
Muhammad-Ohee <md.ohee86@gmail.com>
2024-02-13 13:38:35 +0600

    Add project files.

---
+++ PartialClassAndMethod/PartialEmployeeOne.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClassAndMethod
+{
+    public partial class PartialEmployee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+
+
+        partial void PartialMethod();
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public Method Invoked.");
+            PartialMethod();
+        }
+    }
+}
---
+++ PartialClassAndMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿//Console.WriteLine("Hello, OS");
+
+namespace PartialClassAndMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialEmployee employee = new PartialEmployee
+            {
+                FirstName = "Tom",
+                LastName = "Crusie",
+                Salary = 100000,
+                Gender = "Male"
+            };
+
+            employee.DisplayFullName();
+            employee.DisplayEmployeeDetails();
+
+            employee.PublicMethod();
+        }
+    }
+}
